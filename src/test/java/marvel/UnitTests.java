39c60f4e1c155a1b2c27e85c273f06f7a11a6f18
Flora Chan <hcha8985@uni.sydney.edu.au>
2021-06-08 20:11:06 +1000

    Refactoring offline marvel model behavior to enable loading from cache's valid data - removed deprecated tests

--- src/test/java/marvel/UnitTests.java
+++ src/test/java/marvel/UnitTests.java
@@ -56,28 +56,6 @@ public class UnitTests {
     }
 
     /**
-     * Testing image path retrieval from provided CharacterInfo object using concrete offline model.
-     *
-     * <p>Test using concrete OfflineMarvelModel,
-     * testing behaviour between ModelFacade and InputModel
-     * when model's getImagePathByInfo() is called</p>
-     */
-    @Test
-    public void testInputModelGetThumbnailFullPathOffline(){
-        //uses concrete input model
-        input = new OfflineMarvelModel();
-        output = mock(OutputModel.class);
-        //GIVEN
-        ConfigHandler config = new ConfigHandler(configFilePath);
-        model = new ModelImpl(input, output, config);
-        model.getCharacterInfo("spider-man");
-        //WHEN
-        String path = model.getImagePathByInfo(model.getCurrentCharacter());
-        //THEN
-        assertNull(path);
-    }
-
-    /**
      * Testing for dummy URL retrieved from offline pastebin model.
      */
     @Test
@@ -89,30 +67,6 @@ public class UnitTests {
         model = new ModelImpl(input, output, config);
         //WHEN-THEN
         assertEquals("dummy-report-output-url", model.getReportUrl());
-    }
-
-    /**
-     * Testing for dummy data retrieved from offline marvel model
-     */
-    @Test
-    public void testOfflineMarvelModelGetCharacter(){
-        //GIVEN
-        output = mock(OutputModel.class);
-        input = new OfflineMarvelModel();
-        ConfigHandler config = new ConfigHandler(configFilePath);
-        model = new ModelImpl(input, output, config);
-
-        //WHEN
-        CharacterInfo info = input.getInfoByName("dummy");
-
-        //THEN
-        assertEquals(info.getId(), 123456);
-        assertEquals(info.getName(), "Dummy Hero");
-        assertEquals(info.getNComics(), 1);
-        assertEquals(info.getNSeries(), 3);
-        assertEquals(info.getNStories(), 2);
-        assertEquals(info.getNEvents(), 4);
-        assertEquals(info.getUrls().size(), 3);
     }
 
     /**
